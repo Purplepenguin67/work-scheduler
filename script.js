9592cba7079924a5b36e35008a2783a272c8de90
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-06-05 14:23:43 -0700

    trying to get cvurrent date working

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 //todays date and time in header
 var currentdate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(currentdate);
+
+
+$(document).ready(function () {
+    $(".savebtn").on("click", function () {
+        console.log(this);
+            var input = $(this).siblings(".description").val();
+            
+            var time = $(this).parent().attr("id");
+
+            localStorage.setItem(time, input);
+
+}
+
+

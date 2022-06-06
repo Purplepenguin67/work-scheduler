6d7fdedfc6d91d513f25df41f2255d5d94453503
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-06-05 17:46:50 -0700

    I still cant get the time to work

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 //todays date and time in header
 
 
-//date
-var mydate = new Date();
-var year =mydate.getFullYear();
-if(year < 1000){
-    year +=1900
+var currentdate = moment().format('dddd, MMM Do YYYY');
+
+var today = new Date();
+var dd = today.getDate();
+var mm= today.getMonth()+1;
+var yyyy= today.getfulltear();
+console.log(mm);
+
+if(dd < 10 ){
+        dd = '0' + dd;
 }
-var day = mydate.getDay();
-var month = mydate.getMonth();
-var daym = mydate.getDate();
-var dayarray = new Array("sunday", monday, tuesday, wednesday, thursday, friday, saturday);
-var montharray = new Array(january, febuary, march, april, may, june, july, august, september, october, november, decemeber)
-    
+ if(mm < 10 ){
+    mm = '0' + mm;
+ }
+
+ today = dd + '/' + mm + '/' + yyyy;
+ console.log(today);
+ document.getElementById("time").innerhtml = "today is"+ today
+
+
+
     $(".savebtn").on("click", function () {
         console.log(this);
             var input = $(this).siblings(".description").val();
@@ -22,6 +31,18 @@ var montharray = new Array(january, febuary, march, april, may, june, july, augu
             localStorage.setItem(time, input);
     })
 
+
+//pull from local storage
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
+$("#hour6 .description").val(localStorage.getItem("hour6"));
+$("#hour7 .description").val(localStorage.getItem("hour7"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+
+clocktracker();
 
 
     function clocktracker() {
@@ -52,21 +73,11 @@ var montharray = new Array(january, febuary, march, april, may, june, july, augu
             $(this).removeclass("past");
 
           }
-            })
-            }
-
-          //pull from local storage
-          $("#hour1 .description").val(localStorage.getItem("hour1"));
-          $("#hour2 .description").val(localStorage.getItem("hour2"));
-          $("#hour3 .description").val(localStorage.getItem("hour3"));
-          $("#hour4 .description").val(localStorage.getItem("hour4"));
-          $("#hour5 .description").val(localStorage.getItem("hour5"));
-          $("#hour6 .description").val(localStorage.getItem("hour6"));
-          $("#hour7 .description").val(localStorage.getItem("hour7"));
-          $("#hour8 .description").val(localStorage.getItem("hour8"));
-        
-          clocktracker();
-
         })
+        }
+
+          
+
+    
     
         
